4896be74c3652031bf3f2e4b368d5a41bd03926b
Tanner Nielsen <tan-ali5@outlook.com>
2020-05-16 08:09:28 -0700

    started local storage

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
 $(document).ready(function (){
     // Search for city
     $("#searchButton").on("click", function(){
+        var searchValue = $("#citySearch").val();
+        $("#citySearch").val("");
+        // need to call weather function
         
+    });
+    // Get local storage if any
+    var cityHistory = JSON.parse(localStorage.getItem("history")) || [];
 
-    })
+    
 })
