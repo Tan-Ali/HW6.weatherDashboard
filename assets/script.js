a0c3317ba78a848e094a686c5d902ba60aa13871
Tanner Nielsen <tan-ali5@outlook.com>
2020-05-17 10:02:12 -0700

    formatted

--- assets/script.js
+++ assets/script.js
@@ -38,7 +38,7 @@ $(document).ready(function () {
         $.ajax({
             type: "GET",
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=973fe31b877852d30bc224eba51b5df1&units=imperial",
-          
+
 
         }).then(function (data) {
             if (history.indexOf(searchTerm) === -1) {
@@ -50,7 +50,6 @@ $(document).ready(function () {
 
             var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
             var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-
 
             var card = $("<div>").addClass("card");
             var cardBody = $("<div>").addClass("card-body");
@@ -64,15 +63,14 @@ $(document).ready(function () {
             $.ajax({
                 type: "GET",
                 url: "https://api.openweathermap.org/data/2.5/uvi?appid=973fe31b877852d30bc224eba51b5df1&lat=" + lat + "&lon=" + lon,
-               
+
             }).then(function (response) {
                 console.log(response);
 
-                
+
                 var uvResponse = response.value;
                 var uvIndex = $("<p>").addClass("card-text").text("UV Index: ");
                 var btn = $("<span>").addClass("btn btn-sm").text(uvResponse);
-
 
                 if (uvResponse < 3) {
                     btn.addClass("btn-success");
@@ -100,7 +98,7 @@ $(document).ready(function () {
         $.ajax({
             type: "GET",
             url: "https://api.openweathermap.org/data/2.5/forecast?q=" + searchTerm + "&appid=973fe31b877852d30bc224eba51b5df1&units=imperial",
-          
+
         }).then(function (data) {
             console.log(data);
             $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
@@ -112,13 +110,11 @@ $(document).ready(function () {
                     var titleFive = $("<h3>").addClass("card-title").text(new Date(data.list[i].dt_txt).toLocaleDateString());
                     var imgFive = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
 
-                   
                     var cardFive = $("<div>").addClass("card bg-primary text-white");
                     var cardBodyFive = $("<div>").addClass("card-body p-2");
                     var humidFive = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
                     var tempFive = $("<p>").addClass("card-text").text("Temperature: " + data.list[i].main.temp + " °F");
 
-                    
                     titleFive.append(imgFive);
                     cardBodyFive.append(titleFive, tempFive, humidFive);
                     cardFive.append(cardBodyFive);
